a16510c4d11708f8cf7975b3b5d50eda120497a2
KillByte <killbyte.prototype@gmail.com>
2020-11-07 20:32:03 +0000

    + TPC4

---
+++ tpcs/tpc4/server.js
@@ -0,0 +1,69 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var logger = require('./logger').logger;
+
+logger.info(`Starting server ...`);
+
+
+const PAGES_DIR='pages/'
+const PORT=7777
+const HTTP_HEADER={'Content-Type':'text/html;charset=utf-8;'};
+
+var NUM_ARQ_FILES;
+fs.readdir(PAGES_DIR,(err,files) => {
+    NUM_ARQ_FILES = files.filter(e => e.match(/^arq(([1-9]+)|([1-9]0+)).html$/)).length;
+});
+
+
+function serve_file(res,filename,code=200)
+{
+    fs.readFile(filename,(err,data) =>
+    {
+        res.writeHead(code,HTTP_HEADER);
+        res.write(data);
+        res.end();
+    });
+}
+
+try {
+    http.createServer((req,res) =>
+    {
+        console.log(req.method + " "+req.url + " "+"Todays date");
+
+        let http_code;
+
+        if(req.url.match(/^((\/index)(\.html)?)|\/$/)) 
+        {
+            http_code = 200;
+            serve_file(res,PAGES_DIR + 'index.html');
+        }
+        else if(req.url.match(/^\/arqs\/(([1-9]+)|([1-9]0+))$/))
+        {
+            let arq_num = parseInt(req.url.substring(6, req.url.length));
+            if(arq_num > NUM_ARQ_FILES)
+            {
+                logger.info("URL matched but number doesn't exist");
+                http_code = 404
+                serve_file(res,PAGES_DIR + '404.html',http_code);
+            }
+            else
+            {
+                http_code = 200;
+                serve_file(res,PAGES_DIR + 'arq' + arq_num + '.html');
+            }
+        }
+        else
+        {
+            http_code = 404;
+            serve_file(res,PAGES_DIR + '404.html',http_code);
+        }
+        logger.request(req.method, req.url, http_code);
+
+    }).listen(PORT);
+} 
+catch(err) {
+    logger.error(err);
+}
+
+logger.info(`Server opened in ${PORT}`);
