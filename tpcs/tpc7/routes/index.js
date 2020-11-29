15607f185ad906037945184f102602a4477b0526
KillByte <killbyte.prototype@gmail.com>
2020-11-29 13:58:18 +0000

    Updated TP7

---
+++ tpcs/tpc7/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var Student = require('../controllers/student')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/students', function(req, res) {
+  // Data retrieve
+  Student.list()
+    .then(data => res.render('students', { list: data }))
+    .catch(err => res.render('error', {error: err}))
+  ;
+});
+
+module.exports = router;
