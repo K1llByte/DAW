bd79da1388efc9a5ccd7ffd2f8bdf9632df0de5c
KillByte <killbyte.prototype@gmail.com>
2020-12-08 19:32:14 +0000

    Added TPC8

---
+++ tpcs/tpc8/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const templates = require('./templates/files');
+const jsonfile = require('jsonfile');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({dest: 'uploads_cache/'});
+const { dirname } = require('path')
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('static_files'));
+
+// ================= APP VARS ================= //
+
+const PORT = 8081;
+const HTTP_HEADER = {'Content-Type': 'text/html;charset=utf-8'};
+const FILES_JSON_DB = './db_files.json';
+
+// ================== ROUTES ================== //
+
+app.get('/', (req,res) => {
+    const files = jsonfile.readFileSync(FILES_JSON_DB);
+
+    res.writeHead(200, HTTP_HEADER);
+    res.write(templates.list_files(files));
+    res.end();
+})
+
+app.get('/files/upload', (req,res) => {
+    const date = new Date().toISOString().substr(0,16);
+    res.writeHead(200, HTTP_HEADER);
+    res.write(templates.upload_files(date));
+    res.end();
+})
+
+app.get('/files/download/:fname', (req,res) => {
+    // TODO: REMOVE THIS CODE THIS WILL ALLOW DOWNLOAD
+    // OF ANY FILESYSTEM FILE THE USER HAS ACCESS
+    res.download(__dirname + '/static_files/uploads/' + req.params.fname);
+})
+
+app.post('/files', upload.array('myFile'), (req,res) => {
+
+    for(let i = 0 ; i < req.files.length ; ++i)
+    {
+        const old_path = __dirname + '/' + req.files[i].path;
+        const new_path = __dirname + '/static_files/uploads/' + req.files[i].originalname;
+        fs.rename(old_path, new_path, (err) => {
+            if(err) 
+                throw err;
+        });
+
+        const date = new Date().toISOString().substr(0,16);
+        var files = jsonfile.readFileSync(FILES_JSON_DB);
+        files.push(
+            {
+                date: date,
+                name: req.files[i].originalname,
+                size: req.files[i].size,
+                mimetype: req.files[i].mimetype,
+                description: req.body.desc[i]
+            }
+        );
+        jsonfile.writeFileSync(FILES_JSON_DB, files);
+    }
+    res.redirect('/');
+})
+
+// ================== START SERVER ================== //
+
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT} ...`);
+});
---
+++ tpcs/tpc8/static_files/imagens.js
@@ -0,0 +1,47 @@
+function showImage(name, type, desc){
+    if(type == 'image/png' || type == 'image/jpeg'){
+        var fileObj = $(`
+        <div class="w3-row w3-margin">
+            <div class="w3-col s6">
+                <img src="/uploads/${name}" width="80%"/>
+            </div>
+            <div class="w3-col s6 w3-border">
+                <p>Filename: ${name}</p>
+                <p>Nametype: ${type}</p>
+                <p>Description: ${desc}</p>
+            </div>
+        </div>
+    `)
+    }
+    else
+        var fileObj = $('<p>' + name + ', ' + type + '</p>')
+
+
+    var download = $('<div><a href="/files/download/' + name + '">Download</a></div>"')
+    $("#display").empty()
+    $("#display").append(fileObj, download)
+    $("#display").modal()
+}
+
+function showUpload(){
+    var new_up = $(`
+        <div class="w3-row w3-margin-bottom">
+            <div class="w3-col s3">
+                <label class="w3-text-teal">Description</label>
+            </div>
+            <div class="w3-col.s9.w3-border">
+                <input class="w3-input w3-border w3-light-grey" type="text" name="desc">
+            </div>
+            <div class="w3-row w3-margin-bottom">
+        </div>
+            <div class="w3-col s3">
+                <label class="w3-text-teal">Select file</label>
+            </div>
+            <div class="w3-col.s9.w3-border">
+                <input class="w3-input w3-border w3-light-grey" type="file" name="myFile">
+            </div>
+        </div>
+    `)
+    $("#upload").append(new_up)
+}
+
